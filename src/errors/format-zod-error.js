cc78c25988d1664f63a502b8b461035866bb4936
Donald Geddes <hi@donaldgeddes.ca>
2023-05-25 02:19:30 -0700

    Refactor

--- src/errors/format-zod-error.js
+++ src/errors/format-zod-error.js
@@ -5,16 +5,19 @@ import { ZodError } from 'zod'
  * @param {ZodError} error
  */
 export const formatZodError = error => {
+  /**
+   * @param {[string, string[]]} params
+   */
+  const toLine = ([param, ...causes]) =>
+    '  ' + param + ':' + ' ' + causes.flat().join(' - ')
+
   const formatted = pipe(
     error.format(),
     omitBy((v, k) => k.startsWith('_')),
     // @ts-ignore
     mapValues(val => val._errors),
     toPairs,
-    map(
-      ([param, ...causes]) =>
-        '  ' + param + ':' + ' ' + causes.flat().join(' - ')
-    )
+    map(toLine)
   ).join('\n')
 
   return '\n\n' + formatted + '\n'
